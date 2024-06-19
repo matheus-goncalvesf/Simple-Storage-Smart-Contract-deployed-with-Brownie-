0a3fce094d574a78bfe52d501164e4af82b171f9
Matheus <user.matheus.gon@gmail.com>
2024-06-19 05:21:31 -0300

    add a mapping in it

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -6,23 +6,27 @@ contract SimpleStorage {
 
     uint256 favoriteNumber;
 
-    // This is a comment!
+    // create a "person" type
     struct People {
         uint256 favoriteNumber;
         string name;
     }
 
+    // create a array for a data type called Person and make a mapping over it
     People[] public people;
     mapping(string => uint256) public nameToFavoriteNumber;
 
+    //store a favourite numeber
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
     
+    //return the vavourite number
     function retrieve() public view returns (uint256){
         return favoriteNumber;
     }
 
+    //Make a instance of a Person data type
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
